b343d670eb439fb08691786224c62e0c53833de2
Julia <59516124+JuliaFitzgibbons@users.noreply.github.com>
2021-03-27 20:22:46 -0400

    Add files via upload
    Added exponential fitting routine

--- ExponentialModel.m
+++ ExponentialModel.m
@@ -4,18 +4,46 @@ I0=5;
 
 R0 = 2.5;
 tmax = 4;
-t = 0:1/8:tmax;
+t = (0:1/8:tmax);
 
 
 %These are the data streams ( arrays of outputs)
 I = I0*(R0).^t;
 S = S0 - I0*(R0).^t;
 
-% Plot
 
-plot(t, [I; S]);
-axis([0 tmax 0 100])
+%Adding Gaussian distributed noise where the mean (mu) is 0 and the stdev is 1
+
+mu=0;
+sigma=1;
+noise = sigma *randn(1,length(t))+mu;
+
+noiseyS =  noise + S;
+noiseyI = noise + I;
+
+
+expForm = fittype('a-b*exp(r*x)');
+expFit = fit(t',noiseyI', expForm, 'StartPoint',[1,1,1]);
+
+coeffs = coeffvalues(expFit);
+r = coeffs(3);
+
+
+plot(t, [I;noiseyI], t, expFit(t));
+axis([0 tmax 0 100]);
 title('Exponential Model');
-legend('I(t)','S(t)');
+legend('I(t)', 'I(t) with noise','Exponential fit of I');
 xlabel('Time, t');
 ylabel('Population');
+
+%plot(t, [I;noiseyI; S;noiseyS]);    How to plot with original and noise
+
+% plot(t, [I;noiseyI; S;noiseyS]);
+% axis([0 tmax 0 100])
+% title('Exponential Model');
+% legend('I(t)', 'I(t) with noise','S(t','S(t)with noise');
+% xlabel('Time, t');
+% ylabel('Population');
+
+
+
--- SIRModel.m
+++ SIRModel.m
@@ -9,13 +9,29 @@ tmax = 30;
 beta = 0.01;
 gamma = 0.03;
 
-
 [t,y] = ode45('systemofKM',[0 tmax],[S0; I0; R0]);
+
+%Adding Gaussian distributed noise where the mean (mu) is 0 and the stdev is 1
+
+mu=0;
+sigma=1;
+noise = sigma *randn(length(t),1)+mu;
+
+noisevect= [ noise, noise,noise];
+
+
+newy = noisevect + y;
+
+expForm = fittype('a-b*exp(r*x)');
+expFit = fit(t,newy(:,2), expForm, 'StartPoint',[1,1,1]);
+
+coeffs = coeffvalues(expFit);
+r = coeffs(3);
 
 
 %Figure details
-plot(t, y);
+plot(t, y(:,2), t, newy(:,2), t, expFit(t));
 title('SIR Model');
-legend('S(t)','I(t)','R(t)');
+legend('I(t)', 'I with Noise', 'I exponential fit');
 xlabel('time, t');
 ylabel('Population');
