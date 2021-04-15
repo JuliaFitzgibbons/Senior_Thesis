8aa5147950898d84c73fa05ebee65f94fef67972
Julia <59516124+JuliaFitzgibbons@users.noreply.github.com>
2021-04-14 20:44:01 -0400

    Add files via upload
    Graphing maximum and minimum residuals

--- SIRModel.m
+++ SIRModel.m
@@ -25,7 +25,7 @@ coeffs = coeffvalues(expFit);
 r = coeffs(2);
 
 %Plot SIR and exponential graph
-subplot(3,1,1);
+subplot(4,1,1);
 plot(t, y(:,2), newt, expFit(newt));
 title('SIR Model');
 legend('I(t)', 'I with exponential fit until first inflection point');
@@ -39,54 +39,79 @@ fprintf('%d is Heesterbeck paper general value\n',exp(r/gamma));
 fprintf('%d is Heesterbeck paper small rTG value\n',1+(r/gamma));
 
 %/////////////////////////////// variable  beta and gamma////////////////////////
-ChangingR0 = 1.3; %1.1 : .1: 4.0;
-changingBetaAndGamma= zeros(4,90);
-index = 1;
-maxDiff = 0; %zeros(3,29);
-minDiff = 0; %zeros(3,29);
-for i = .0005:0.00005:.005
-    beta = i;
-    gamma = beta/(ChangingR0 /100);
-    [t,y] = ode45('systemofKM',[0 tmax],[S0; I0; R0]);
-    yI = y(:,2);
-    inflection_idx = find(diff(sign(gradient(gradient(yI)))));
-    newt = t(1:inflection_idx(1));
-    expForm = fittype('b*exp(r*x)');
-    expFit = fit(newt,yI(1:inflection_idx(1)), expForm, 'StartPoint',[1,.1]);
-    coeffs = coeffvalues(expFit);
-    r = coeffs(2);
-    
-    %fprintf('r estimated : %d\n',r);
-    
-    
-    subplot(3,1,2);
-    plot(t, y(:,2), newt, expFit(newt));
-    title('SIR Model');
-    legend('I(t)', 'I(t) with exponential fit until first inflection point');
-    xlabel('Time, t');
-    ylabel('Population');
+    maxDiff = zeros(3,29); %First exp, then heesterbeek small, then heesterbeek large
+    minDiff = 100*ones(3,29);
+    count = 1;
+for ChangingR0 = 1.2 :0.1:4.0
+    changingBetaAndGamma= zeros(4,90);
+    index = 1;
+    for i = .0005:0.00005:.005
+        beta = i;
+        gamma = beta/(ChangingR0 /100);
+        [t,y] = ode45('systemofKM',[0 tmax],[S0; I0; R0]);
+        yI = y(:,2);
+        inflection_idx = find(diff(sign(gradient(gradient(yI)))));
+        newt = t(1:inflection_idx(1));
+        expForm = fittype('b*exp(r*x)');
+        expFit = fit(newt,yI(1:inflection_idx(1)), expForm, 'StartPoint',[1,.1]);
+        coeffs = coeffvalues(expFit);
+        r = coeffs(2);
+
+        %fprintf('r estimated : %d\n',r);
+
+
+        subplot(4,1,2);
+        plot(t, y(:,2), newt, expFit(newt));
+        title('SIR Model');
+        legend('I(t)', 'I(t) with exponential fit until first inflection point');
+        xlabel('Time, t');
+        ylabel('Population');
+
+        changingBetaAndGamma(1,index)= exp(r); % e^r from the model
+        changingBetaAndGamma(2,index)= beta*S0/gamma; %From Kermack-Mckendrick ???????
+        changingBetaAndGamma(3,index)= exp(r/gamma); % from the Heesterbeek paper
+        changingBetaAndGamma(4,index)= 1+(r/gamma); %from the Heesterbeek paper, where r is exp growth and
+        %TG is 1/beta by my calculation
         
-    changingBetaAndGamma(1,index)= exp(r); % e^r from the model
-    changingBetaAndGamma(2,index)= beta*S0/gamma; %From Kermack-Mckendrick ???????
-    changingBetaAndGamma(3,index)= exp(r/gamma); % from the Heesterbeek paper
-    changingBetaAndGamma(4,index)= 1+(r/gamma); %from the Heesterbeek paper, where r is exp growth and
-    %TG is 1/beta by my calculation
-    index = index + 1;
-    
-    if (maxDiff< abs(1+(r/gamma)- ChangingR0))
-        maxDiff = abs(1+(r/gamma)- ChangingR0);
+
+        if (maxDiff(1,count)< abs(exp(r) - ChangingR0))
+            maxDiff(1,count) = abs(exp(r)- ChangingR0);
+        end
+        if (minDiff(1,count)> abs(exp(r)- ChangingR0))
+            minDiff(1,count) = abs(exp(r)- ChangingR0);
+        end
+        
+        if (maxDiff(2,count)< abs(1+(r/gamma) - ChangingR0))
+            maxDiff(2,count) = abs(1+(r/gamma)- ChangingR0);
+        end
+        if (minDiff(2,count)> abs(1+(r/gamma)- ChangingR0))
+            minDiff(2,count) = abs(1+(r/gamma)- ChangingR0);
+        end
+        if (maxDiff(3,count)< abs(exp(r/gamma) - ChangingR0))
+            maxDiff(3,count) = abs(exp(r/gamma)- ChangingR0);
+        end
+        if (minDiff(3,count)> abs(exp(r/gamma)- ChangingR0))
+            minDiff(3,count) = abs(exp(r/gamma)- ChangingR0);
+        end
+        
+        index = index + 1;
     end
-    if (minDiff> abs(1+(r/gamma)- ChangingR0))
-        minDiff = abs(1+(r/gamma)- ChangingR0);
-    end
+    count = count + 1;
 end
 
 %Figure details
-subplot(3,1,3);
-%plot(.0001:0.0001:.01, [betachanging(2,:); betachanging(3,:)]);
-plot(.0005:0.00005:.005, changingBetaAndGamma);
-title('R0 values');
-legend( 'Proposed R0','KM R0(constant 1.5)', 'Heesterbeek R0','Small Heesterbeek R0');
-xlabel('Beta value');
-ylabel('value');
+subplot(4,1,3);
+plot(1.2 :0.1:4.0, maxDiff);
+title('Maximum Residuals');
+legend( 'e^(r)','1+(r/gamma)', '1+e^(r/gamma)');
+xlabel('R0');
+ylabel('Residual from R0');
+
+%Figure details
+subplot(4,1,4);
+plot(1.2 :0.1:2.0, minDiff);
+title('Minimum Residuals');
+legend( 'e^(r)','1+(r/gamma)', '1+e^(r/gamma)');
+xlabel('R0');
+ylabel('Residual from R0');
 
